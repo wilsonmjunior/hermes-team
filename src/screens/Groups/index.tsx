8e019beb5e3a4369cb105ed4c6b1b1916861699c
Wilson Junior <wilson.junior@merx.tech>
2023-04-23 12:18:48 -0300

    feat: create header

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,11 +1,11 @@
-import { Text } from "react-native";
-
 import { Container } from "./styles";
+
+import { Header } from "@components/Header";
 
 export function Groups() {
   return (
     <Container>
-      <Text>Groups</Text>
+      <Header />
     </Container>
   )
 }
