f9a0c9c38689a64a46dc09b412568d506855483b
Wilson Junior <wilson.junior@merx.tech>
2023-04-22 23:04:06 -0300

    feat: load fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,38 @@
-import { Groups } from '@screens/Groups';
+import * as SplashScreen from 'expo-splash-screen';
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts
+} from '@expo-google-fonts/roboto';
 
 import { View } from 'react-native';
 import { ThemeProvider } from 'styled-components/native';
 
+import { Groups } from '@screens/Groups';
 import { theme } from './theme';
+import { useCallback } from 'react';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+  
   return (
     <ThemeProvider theme={theme}>
-      <View>
+      <View onLayout={onLayoutRootView}>
         <Groups />
       </View>
     </ThemeProvider>
