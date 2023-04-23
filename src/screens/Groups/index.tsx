b2887fa7ce4570949de7b903c64d6169d1f60e61
Wilson Junior <wilson.junior@merx.tech>
2023-04-23 19:10:12 -0300

    feat: create list groups

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,18 @@
-import { Highlight } from "@components/Highlight";
 import { Header } from "@components/Header";
-
-import { Container, HeaderWrapper } from "./styles";
+import { Highlight } from "@components/Highlight";
 import { GroupCard } from "@components/GroupCard";
 
+import { Container, HeaderWrapper } from "./styles";
+import { useState } from "react";
+import { FlatList } from "react-native";
+
 export function Groups() {
+  const [groups, setGroups] = useState([
+    'Galera do ignite',
+    'Galera do falcone',
+    'Galera do pep',
+  ]);
+
   return (
     <Container>
       <HeaderWrapper>
@@ -16,7 +24,11 @@ export function Groups() {
         subtitle="Jogue com a sua turma"
       />
 
-      <GroupCard title="Nome da turma" />
+      <FlatList 
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+      />
     </Container>
   )
 }
