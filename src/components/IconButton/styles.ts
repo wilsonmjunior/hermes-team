caa73ec91d052eb34e2294cd91d5aabc89fdc691
Wilson Junior <wilson.junior@merx.tech>
2023-04-23 23:31:32 -0300

    feat: create icon button component

---
+++ src/components/IconButton/styles.ts
@@ -0,0 +1,26 @@
+import { MaterialIcons } from '@expo/vector-icons';
+import { TouchableOpacity } from "react-native";
+import styled from "styled-components/native";
+
+export type IconButtonTypeStyleProps = 'primary' | 'secondary';
+
+type IconTypeProps = {
+  type: IconButtonTypeStyleProps;
+}
+
+export const Container = styled(TouchableOpacity)`
+  width: 56px;
+  height: 56px;
+
+  justify-content: center;
+  align-items: center;
+
+  margin-left: 12px;
+`;
+
+export const Icon = styled(MaterialIcons).attrs<IconTypeProps>(({ theme, type }) => {
+  return ({
+    size: theme.FONT_SIZE.LG,
+    color: type === 'primary' ? theme.COLORS.GREEN_700 : theme.COLORS.RED_DARK,
+  })
+})``;
